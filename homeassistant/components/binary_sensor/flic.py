091aaaabdd9f97e88034f3d5ab5a7f8df7eb1629
theit8514 <theit8514@users.noreply.github.com>
2018-07-22 01:27:14 -0400

    Restart flic client after socket disconnects

--- homeassistant/components/binary_sensor/flic.py
+++ homeassistant/components/binary_sensor/flic.py
@@ -6,6 +6,7 @@ https://home-assistant.io/components/binary_sensor.flic/
 """
 import logging
 import threading
+import time
 
 import voluptuous as vol
 
@@ -21,6 +22,8 @@ REQUIREMENTS = ['pyflic-homeassistant==0.4.dev0']
 _LOGGER = logging.getLogger(__name__)
 
 DEFAULT_TIMEOUT = 3
+
+DATA_FLIC = 'data_flic'
 
 CLICK_TYPE_SINGLE = 'single'
 CLICK_TYPE_DOUBLE = 'double'
@@ -47,37 +50,15 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
         vol.All(cv.ensure_list, [vol.In(CLICK_TYPES)])
 })
 
-
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the flic platform."""
-    import pyflic
-
-    # Initialize flic client responsible for
-    # connecting to buttons and retrieving events
-    host = config.get(CONF_HOST)
-    port = config.get(CONF_PORT)
-    discovery = config.get(CONF_DISCOVERY)
-
-    try:
-        client = pyflic.FlicClient(host, port)
-    except ConnectionRefusedError:
+    hass.data[DATA_FLIC] = []
+    client = connect_client(config)
+    if client is None:
         _LOGGER.error("Failed to connect to flic server")
         return
 
-    def new_button_callback(address):
-        """Set up newly verified button as device in Home Assistant."""
-        setup_button(hass, config, add_entities, client, address)
-
-    client.on_new_verified_button = new_button_callback
-    if discovery:
-        start_scanning(config, add_entities, client)
-
-    hass.bus.listen_once(EVENT_HOMEASSISTANT_STOP,
-                         lambda event: client.close())
-
-    # Start the pyflic event handling thread
-    threading.Thread(target=client.handle_events).start()
-
+    setup_client(client, hass, config, add_entities, discovery_info)
     def get_info_callback(items):
         """Add entities for already verified buttons."""
         addresses = items['bd_addr_of_verified_buttons'] or []
@@ -87,6 +68,61 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
     # Get addresses of already verified buttons
     client.get_info(get_info_callback)
 
+def handle_events(client, hass, config, add_entities, discovery_info):
+    client.handle_events()
+    entities = hass.data[DATA_FLIC]
+    # when handle_events exits, the socket has been closed
+    # this should be replaced by an event in the library
+    _LOGGER.warning("Socket closed to flic server")
+    for entity in entities:
+        entity.disconnect()
+    success = False
+
+    while True:
+        time.sleep(5)
+        _LOGGER.info("Attempting connection to flic server")
+        client = connect_client(config)
+        if client is not None:
+            break
+
+    # at this phase, we set the client up again with add_entities, but we don't
+    # add any entities. we find the buttons already set up and connect them to
+    # the new client.
+    setup_client(client, hass, config, add_entities, discovery_info)
+    _LOGGER.info("Reconnected to flic server... reconnecting buttons to client")
+    for entity in entities:
+        entity.connect(client)
+
+def connect_client(config):
+    import pyflic
+    # Initialize flic client responsible for
+    # connecting to buttons and retrieving events
+    host = config.get(CONF_HOST)
+    port = config.get(CONF_PORT)
+    try:
+        client = pyflic.FlicClient(host, port)
+    except:
+        _LOGGER.exception("Failed to connect to flic server")
+        return None
+
+    return client
+
+def setup_client(client, hass, config, add_entities, discovery_info):
+    """Configures the client callbacks and registers hass events"""
+    def new_button_callback(address):
+        """Set up newly verified button as device in Home Assistant."""
+        button = setup_button(hass, config, add_entities, client, address)
+
+    client.on_new_verified_button = new_button_callback
+    discovery = config.get(CONF_DISCOVERY)
+    if discovery:
+        start_scanning(config, add_entities, client)
+
+    hass.bus.listen_once(EVENT_HOMEASSISTANT_STOP,
+                         lambda event: client.close())
+
+    # Start the pyflic event handling thread
+    threading.Thread(target=handle_events, args=[client, hass, config, add_entities, discovery_info]).start()
 
 def start_scanning(config, add_entities, client):
     """Start a new flic client for scanning and connecting to new buttons."""
@@ -113,8 +149,11 @@ def setup_button(hass, config, add_entities, client, address):
     """Set up a single button device."""
     timeout = config.get(CONF_TIMEOUT)
     ignored_click_types = config.get(CONF_IGNORED_CLICK_TYPES)
-    button = FlicButton(hass, client, address, timeout, ignored_click_types)
+    button = FlicButton(hass, address, timeout, ignored_click_types)
+    button.connect(client)
     _LOGGER.info("Connected to button %s", address)
+    entities = hass.data[DATA_FLIC]
+    entities.append(button)
 
     add_entities([button])
 
@@ -122,7 +161,7 @@ def setup_button(hass, config, add_entities, client, address):
 class FlicButton(BinarySensorDevice):
     """Representation of a flic button."""
 
-    def __init__(self, hass, client, address, timeout, ignored_click_types):
+    def __init__(self, hass, address, timeout, ignored_click_types):
         """Initialize the flic button."""
         import pyflic
 
@@ -138,8 +177,8 @@ class FlicButton(BinarySensorDevice):
             pyflic.ClickType.ButtonHold: CLICK_TYPE_HOLD,
         }
 
+        self._is_connected = False
         self._channel = self._create_channel()
-        client.add_connection_channel(self._channel)
 
     def _create_channel(self):
         """Create a new connection channel to the button."""
@@ -163,6 +202,17 @@ class FlicButton(BinarySensorDevice):
             channel.on_button_single_or_double_click_or_hold = self._on_click
 
         return channel
+
+    def disconnect(self):
+        self._is_connected = False
+
+    def connect(self, client):
+        self._is_connected = True
+        client.add_connection_channel(self._channel)
+
+    @property
+    def available(self):
+        return self._is_connected
 
     @property
     def name(self):
